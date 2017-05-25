c1d6d9375f0500cf975d745b266fbe9a6c6f4ced
David V. Lu <davidvlu@gmail.com>
2017-05-25 13:54:25 -0400

    Python

--- ros-jedi.js
+++ ros-jedi.js
@@ -1,13 +1,24 @@
 'use babel'
 
+import { extname } from 'path';
+
 let helpers = null;
 
-const regex = new RegExp([
+const cpp_regex = new RegExp([
   '^(<stdin>|.+):', // Path, usually <stdin>
   '(\\d+):\\s*', // Line Number
   '(.+)', // Main Text
   '\\s*\\[(.+)\\]', // category
   '\\s*\\[(.+)\\]', // certainty
+  '$', // End of fix-it block
+].join(''));
+
+const py_regex = new RegExp([
+  '^(<stdin>|.+):', // Path, usually <stdin>
+  '(\\d+):\\s*', // Line Number
+  '(\\d+):\\s*', // Col Number
+  '([EW])(\\d+)\\s+', // Categorization
+  '(.+)', // Main Text
   '$', // End of fix-it block
 ].join(''));
 
@@ -24,37 +35,73 @@ export function provideLinter() {
     name: 'ROS Jedi',
     scope: 'file',
     lintsOnChange: false,
-    grammarScopes: ['source.cpp', 'source.h'],
+    grammarScopes: ['source.cpp', 'source.h', 'source.python'],
     lint(textEditor) {
       if (helpers === null) {
         helpers = require('atom-linter');
       }
-      const editorPath = textEditor.getPath()
-      return helpers.exec('/opt/ros/indigo/lib/roslint/cpplint', [editorPath]).then(function(a) {
-          return [];
-      }, function(a) {
-          var toReturn = [];
-          var str_array = String(a).split('\n');
-          for(var i = 0; i < str_array.length-2; i++) { // Minus 2 to skip done processing and total errors
-              let match = regex.exec(str_array[i]);
-              if(match == null){
-                  console.log(str_array[i]);
-              }else{
-                  var line = Number.parseInt(match[2])-1;
-                  toReturn.push({
-                    severity: 'error',
-                    location: {
-                      file: editorPath,
-                      position: [[line, 0], [line, 1]],
-                    },
-                    excerpt: match[3],
-                    description: match[4],
-                  });
+      const editorPath = textEditor.getPath();
+      if (typeof editorPath === 'undefined') {
+        // The editor has no path, meaning it hasn't been saved.
+        return [];
+      }
+      const fileExt = extname(editorPath);
+      if(fileExt == '.py'){
+          return helpers.exec('/opt/ros/indigo/lib/roslint/pep8', [editorPath], {ignoreExitCode: true}).then(function(a) {
+              var toReturn = [];
+              var str_array = String(a).split('\n');
+              for(var i = 0; i < str_array.length; i++) {
+                  let match = py_regex.exec(str_array[i]);
+                  if(match == null){
+                      console.log(str_array[i]);
+                  }else{
+                      var line = Number.parseInt(match[2])-1;
+                      var col = Number.parseInt(match[3])-1;
+                      toReturn.push({
+                        severity: match[4]=='E'?'error':'warning',
+                        location: {
+                          file: editorPath,
+                          position: [[line, col], [line, col+1]],
+                        },
+                        excerpt: match[6],
+                        description: match[6],
+                      });
+                      //console.log(toReturn[toReturn.length-1]);
+                  }
               }
-          }
-          return toReturn;
+              return toReturn;
+          }, function(a) {
+              console.log("X");
+              return [];
+          });
 
-      });
+      }else{
+          return helpers.exec('/opt/ros/indigo/lib/roslint/cpplint', [editorPath]).then(function(a) {
+              return [];
+          }, function(a) {
+              var toReturn = [];
+              var str_array = String(a).split('\n');
+              for(var i = 0; i < str_array.length-2; i++) { // Minus 2 to skip done processing and total errors
+                  let match = cpp_regex.exec(str_array[i]);
+                  if(match == null){
+                      console.log(str_array[i]);
+                  }else{
+                      var line = Number.parseInt(match[2])-1;
+                      toReturn.push({
+                        severity: 'error',
+                        location: {
+                          file: editorPath,
+                          position: [[line, 0], [line, 1]],
+                        },
+                        excerpt: match[3],
+                        description: match[4],
+                      });
+                  }
+              }
+              return toReturn;
+
+          });
+      }
     }
   }
 }
